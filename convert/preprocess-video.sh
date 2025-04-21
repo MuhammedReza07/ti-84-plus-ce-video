300a98b7af5d54aa3249229ea073bfb359899b6f
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2025-04-21 23:34:46 +0200

    Added .gitignore, finished first part of convert.sh

--- convert/preprocess-video.sh
+++ convert/preprocess-video.sh
@@ -2,8 +2,10 @@
 
 # This is a preprocessing script used to resize and resample a video.
 # Syntax: preprocess-video <input file path> <width>x<height> <fps> <output file path>
+#
 # To convert a video to the format required by the conversion utility, use the following command.
-# preprocess-video <input file path> 320x240 15 <output file path>
+# preprocess-video.sh <input file path> 320x240 15 <output file path>
+#
 # Note that the frame rate may be modified to ensure that the video fits on the calculator.
 
 input=$1
