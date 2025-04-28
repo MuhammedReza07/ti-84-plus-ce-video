aacc2dbeb19869b5e57ce4b09260238502625aa2
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2025-04-28 11:35:12 +0200

    Modified scripts to accommodate variable frame rates. Also removed convert.sh

--- convert/preprocess-video.sh
+++ convert/preprocess-video.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # This is a preprocessing script used to resize and resample a video.
-# Syntax: preprocess-video <input file path> <width>x<height> <fps> <output file path>
+# Syntax: preprocess-video.sh <input file path> <width>x<height> <fps> <output file path>
 #
-# To convert a video to the format required by the conversion utility, use the following command.
-# preprocess-video.sh <input file path> 320x240 15 <output file path>
+# To convert the video such that its dimensions are compatible with the TI-84 Plus CE LCD display,
+# the following command may be used.
+# preprocess-video.sh <input file path> 320x240 <fps> <output file path>
 #
-# Note that the frame rate may be modified to ensure that the video fits on the calculator.
+# Note that the frame rate must be set such that the video fits on the calculator if the video
+# is going to be played on the TI-84 Plus CE.
 
 input=$1
 size=$2
