b3cf5688dc5a46dfcb79ab5c4a01a668911781a9
lukasbro <lukasbro@kth.se>
2025-05-07 11:36:30 +0200

    step towards simulating pixels using ASCII halfblocks, gives smaller player than intended :(

---
+++ player/draw_matrix.py
@@ -0,0 +1,49 @@
+import math
+
+# let the input matrix define the size later
+ROWS = -1    # should be max 60
+COLUMNS = -1 # should be max 128
+
+def choose_block(top, bottom): # [top, bottom]
+    character=""
+    match [top, bottom]:
+        case [1,0]:
+            character="▀"
+        case [0,1]:
+            character="▄"
+        case [1,1]:
+            character="█"
+        case [0,0]:
+            character=" "
+        case _:
+            character="x"  # something went wrong if this appears
+    return character
+
+def create_pixel_matrix(matrix):     # assume matrix is (not) structured like [column, column2, ...]
+    # update matrix sizes
+    ROWS = len(matrix)
+    COLUMNS = len(matrix[0])
+    pixel_matrix = []
+    for r in range(math.floor(ROWS/2)):
+        pixel_matrix.append("")
+        for c in range(COLUMNS):
+            pixel_matrix[r] += choose_block(matrix[r*2][c], matrix[r*2+1][c])
+    return pixel_matrix
+   
+
+def draw(pixel_matrix):   # draw the matrix
+    for row in pixel_matrix:
+        print(row)
+
+
+
+# run tests
+
+print("start!")
+print(choose_block(0,0))
+matrix = [[1, 0, 1], [0, 1, 0], [0, 0, 0], [1, 1, 0]]
+pm = create_pixel_matrix(matrix)
+    
+#matrix.append([1,1,1])
+#print(matrix, "wow"+"WOW")
+draw(pm)
